232ced5bc64d0c26005408569fad628ae7906827
Alice <alicelu625@gmail.com>
2021-01-27 23:02:47 -0800

    implemented backdrop with modal

--- src/shared/Backdrop/Backdrop.js
+++ src/shared/Backdrop/Backdrop.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Backdrop.module.css';
 
 const backdrop = (props) => (
-    props.show 
+    props.showBackdrop 
         ? <div className={classes.Backdrop} onClick={props.backdropClicked}></div>
         : null
 );
--- src/shared/Modal/Modal.js
+++ src/shared/Modal/Modal.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+
+import Backdrop from '../Backdrop/Backdrop';
+import classes from './Modal.module.css';
 
 const modal = (props) => {
     return (
-        <div
-            style={{
-                transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.showModal ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
+        <Fragment>
+            <Backdrop showBackdrop={props.showModal} backdropClicked={props.modalClosed}/>
+            <div
+                className={classes.Modal}
+                style={{
+                    transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.showModal ? '1' : '0'
+                }}
+            >
+                {props.children}
+            </div>
+        </Fragment>
     );
 }
 
