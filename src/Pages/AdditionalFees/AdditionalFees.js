02fd1cedd231754dd8ee7fff2c26ac74242a6122
Alice <alicelu625@gmail.com>
2021-07-07 23:08:33 -0700

    cleanup

--- src/Pages/AdditionalFees/AdditionalFees.js
+++ src/Pages/AdditionalFees/AdditionalFees.js
@@ -86,7 +86,6 @@ class AdditionalFees extends Component {
         //set caret to last position
         let new_length = val.length;
         caret_position = new_length - original_len + caret_position;
-        console.log(caret_position);
 
         //update state to new updated copy & set caret
         this.setState({ fees },
