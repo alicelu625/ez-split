6be9c14baa2342f3d77c028dd98c08fc2b6cfdc8
Alice <alicelu625@gmail.com>
2022-04-07 21:57:04 -0700

    styled receipt page

--- src/Pages/Receipt/Receipt.js
+++ src/Pages/Receipt/Receipt.js
@@ -6,24 +6,38 @@ import * as actionTypes from '../../store/actions'
 
 class Receipt extends Component {
     render() {
-        return(
+        return (
             <div className={classes.Receipt}>
-                <div>
-                    {this.props.items.map((item, id) => 
-                        <div key={id}>{item.name}: {item.price.toFixed(2)}</div>
-                    )}
-                </div>
-                <div>
-                    <div>Subtotal: {this.props.subtotal.toFixed(2)}</div>
-                    {this.props.fees.map((fee, id) => (
-                        <div key={id}>
-                            {fee.name}: {parseFloat(fee.amount).toFixed(2)}
+                <div className={classes.ReceiptItems}>
+                    <div className={classes.Items}>
+                        {this.props.items.map((item, id) =>
+                            <div className={classes.Item} key={id}>
+                                <span>{item.name}</span>
+                                <span>${item.price.toFixed(2)}</span>
+                            </div>
+                        )}
+                    </div>
+                    <div className={classes.FeesAndTotals}>
+                        <div className={classes.FeeItem}>
+                            <span className={classes.FeeName}>Subtotal</span>
+                            <span className={classes.Amount}>${this.props.subtotal.toFixed(2)}</span>
                         </div>
-                    ))}
-                    <div>Grand Total: {this.props.total.toFixed(2)}</div>
+                        {this.props.fees.map((fee, id) => (
+                            <div className={classes.FeeItem} key={id}>
+                                <span className={classes.FeeName}>{fee.name}</span>
+                                <span className={classes.Amount}>${parseFloat(fee.amount).toFixed(2)}</span>
+                            </div>
+                        ))}
+                        <div className={classes.GrandTotal}>
+                            <span className={classes.GrandTotalText}>Grand Total</span>
+                            <span className={classes.Amount}>{this.props.total.toFixed(2)}</span>
+                        </div>
+                    </div>
                 </div>
-                <button onClick={this.props.onGoBack}>Back to my bill</button>
-                <button>Close</button>
+                <div className={classes.Buttons}>
+                <button className={classes.BackToBillButton} onClick={this.props.onGoBack}>Back to my bill</button>
+                <button className={classes.CloseButton}>Close</button>
+                </div>
             </div>
         )
     }
