1b7ed195cb01aca257b6790d87f4f48016ab94e7
Alice <alicelu625@gmail.com>
2022-08-17 20:04:39 -0700

    updated back button

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -5,7 +5,7 @@ import classes from './Header.module.css';
 const header = (props) => {
     return (
         <div className={classes.Header}>
-            <button onClick={props.backClicked} className={classes.BackButton}>Back</button>
+            <button onClick={props.backClicked} className={classes.BackButton}>&lt;</button>
             <span className={classes.EventName}>{props.eventName}</span>
         </div>
     );
